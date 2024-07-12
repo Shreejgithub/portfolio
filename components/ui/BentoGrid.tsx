0ae02331b08d6a3dc3f3ea9481183e13fddd0b71
Shreyas Jadhav <126581827+Shreejgithub@users.noreply.github.com>
2024-07-12 12:20:11 +0530

    Footer Section is developed

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -115,7 +115,7 @@ export const BentoGridItem = ({
           {id === 3 && (
             <div className="flex gap-1 lg:gap-5 w-fit absolute -right-3 lg:-right-2">
               <div className="flex flex-col gap-3 lg:gap-8">
-                {['React.js', 'Next.js', 'TypeScript'].map
+                {['React.js', 'Javascript', 'HTML & CSS'].map
                 ((item) => (
                   <span key={item} className="py-2 lg:py-4 lg:px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E]">
                     {item}
@@ -125,7 +125,7 @@ export const BentoGridItem = ({
               </div>
               <div className="flex flex-col gap-3 lg:gap-8">
               <span className="py-4 px-3 rounded-lg text-center bg-[#10132e]"/>
-                {['VueJS', 'AWS', 'MongoDB'].map
+                {['Firebase', 'Node.js', 'MongoDB'].map
                 ((item) => (
                   <span key={item} className="py-2 lg:py-4 lg:px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E]">
                     {item}
---
+++ sentry.edge.config.ts
@@ -0,0 +1,16 @@
+// This file configures the initialization of Sentry for edge features (middleware, edge routes, and so on).
+// The config you add here will be used whenever one of the edge features is loaded.
+// Note that this config is unrelated to the Vercel Edge Runtime and is also required when running locally.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://413260ad28b43760478e1e74820618c5@o4507582288953344.ingest.us.sentry.io/4507582314512384",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
